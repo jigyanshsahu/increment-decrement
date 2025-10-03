71b701a7abb51c118f1d9e734cd51587fe918c1a
Jigynash sahu <jigyanshsahu8@gmail.com>
2025-10-03 15:43:24 +0530

    make website responsive for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   const [num, setnum] = useState(0)
   return (
     <div className="center-container">
-      <h3>Number is love you {num}</h3>
+      <h3>Number is{num}</h3>
       <button onClick={()=>setnum(num+1)}> increment</button>
    <button onClick={()=>setnum(num-1)}>decrement</button>
     </div>
